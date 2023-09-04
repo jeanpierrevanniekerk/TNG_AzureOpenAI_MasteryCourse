b91afae4bb5a53f7791261c6d0089fbda847a3f8
jeanpierrevanniekerk <139854126+jeanpierrevanniekerk@users.noreply.github.com>
2023-09-04 21:34:25 +0200

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+sudo echo OPENAI_API_KEY='7079b53b72df4f04bf94a302697561e9' >> /etc/environment
+sudo echo OPENAI_API_BASE="https://tngpocazureopenai-services.openai.azure.com/" >> /etc/environment
+sudo echo OPENAI_API_KEY_WESTEU='1b252da53d8b40b4b9e3de00bf7bc6ab' >> /etc/environment
+sudo echo OPENAI_API_WESTEU_BASE='https://tngpocazureopenai-services-westeurope.openai.azure.com/' >> /etc/environment
+sudo echo REDIS_HOST='tngpocazureopenia.eastus.redisenterprise.cache.azure.net' >> /etc/environment
+sudo echo REDIS_PASSWORD='xyy5fL6sTyOsktuiSlnCVgN+5oD+cRalaNuh2UMxHtE=' >> /etc/environment
